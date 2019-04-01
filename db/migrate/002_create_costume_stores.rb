6818fdac9312e3a16a9c9b7f967430353204f3d9
Jennie Kerfstedt <jenniekerf@gmail.com>
2019-04-01 21:42:06 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,20 @@
+class CreateCostumes < ActiveRecord::Migration[5.2]
+  
+  def change
+    create_table :costumes do 
+      |c|
+      c.string :name
+      c.integer :price
+      c.string :image_url
+      c.string :size 
+      c.datetime :created_at
+      c.datetime :updated_at
+    end
+  end 
+end  
+  
+  
+
+
+
 # Create your costume_stores migration here
