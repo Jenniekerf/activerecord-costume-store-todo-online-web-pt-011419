e3a757ecd339ece599fcdf02090ecc796d5a9096
Jennie Kerfstedt <jenniekerf@gmail.com>
2019-04-01 21:43:47 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,14 +1,10 @@
 class CreateCostumeStores < ActiveRecord::Migration[5.2]
   
   def change
-    create_table :costumes do 
-      |c|
-      c.string :name
-      c.integer :price
-      c.string :image_url
-      c.string :size 
-      c.datetime :created_at
-      c.datetime :updated_at
+    create_table do |t|
+      t.string :name 
+      t.string :location
+      t.
     end
   end 
 end  
