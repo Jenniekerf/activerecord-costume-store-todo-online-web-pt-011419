9d4c57ff60e6de3a88ddb6ab9a34efa8c7fb2388
Jennie Kerfstedt <jenniekerf@gmail.com>
2019-04-01 21:45:40 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -4,7 +4,11 @@ class CreateCostumeStores < ActiveRecord::Migration[5.2]
     create_table do |t|
       t.string :name 
       t.string :location
-      t.
+      t.integer :costume_inventory
+      t.integer :number_of_employees
+      t.boylan :still_in_business?
+      t.datetime :opening_time
+      t.datetime :closing_time
     end
   end 
 end  
